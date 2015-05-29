c03762d4921e124eb7661693ea3f60cfd698eb9b
Theo Pack <tf.pack@gmail.com>
2015-05-29 20:20:15 +0200

    Change bounce

--- bounce.erl
+++ bounce.erl
@@ -1,9 +1,15 @@
 -module(bounce).
--export([report/0]).
+-export([report/0, report/1]).
 
 report() ->
   receive
     X -> io:format("Received ~p~n", [X]),
     report()
+  end.
+
+report(Count) ->
+  receive
+    X -> io:format("Received ~p. Count: ~p", [X,Count]),
+    report(Count + 1)
   end.
 
